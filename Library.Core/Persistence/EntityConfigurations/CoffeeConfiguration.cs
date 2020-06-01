655b656b602605c33ac70ce9062100860c30f811
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:23:34 +0300

    Create CoffeeConfiguration.cs

---
+++ Library.Core/Persistence/EntityConfigurations/CoffeeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Library.Core.Concrete.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Library.Core.Persistence.EntityConfigurations
+{
+    public class CoffeeConfiguration : IEntityTypeConfiguration<Coffee>
+    {
+        public void Configure(EntityTypeBuilder<Coffee> builder)
+        {
+            builder.Property(b => b.Name)
+                .IsRequired()
+                .HasMaxLength(255)
+                .HasColumnName("Brand");
+
+            builder.HasKey(b => b.Id);
+
+            builder
+                .Property(b => b.Id)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+        }
+    }
+}
