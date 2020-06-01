6ffd0124267b95ca0cedf2a2afb0be1267311b85
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:22:57 +0300

    Create IRepository.cs

---
+++ Library.Core/Abstract/Repositories/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Library.Core.Abstract.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(int id);
+        ValueTask<TEntity> GetAsync(int id);
+
+        IEnumerable<TEntity> GetAll();
+        Task<List<TEntity>> GetAllAsync();
+ 
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+        TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+
+        void Remove(TEntity entity);
+
+        int SaveChanges();
+        Task<int> SaveChangesAsync();
+    }
+}
