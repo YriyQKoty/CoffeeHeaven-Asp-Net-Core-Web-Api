19eabd751a31452d4e08fbd8bb56e25c42ec16d4
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:22:39 +0300

    Create CoffeeRequestValidator.cs

---
+++ Library.Api/Validators/CoffeeRequestValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Library.Api.Requests;
+
+namespace Library.Api.Validators
+{
+    public class CoffeeRequestValidator : AbstractValidator<CoffeeRequest>
+    {
+        public CoffeeRequestValidator()
+        {
+            RuleFor(c => c.Id).GreaterThan(0).WithMessage("Coffee ID cannot be negative");
+            
+            RuleFor(c => c.Name).MinimumLength(3).WithMessage("Name is too short");
+            RuleFor(c => c.Name).MaximumLength(64).WithMessage("Name is too long");
+
+            RuleFor(c => c.Price).InclusiveBetween(25, 5000).WithMessage("Price should be in range 25-5000!");
+
+            RuleFor(c => c.Description).MinimumLength(36)
+                .WithMessage("Description should be greater than 36 characters");
+            
+            RuleFor(c => c.Description).MaximumLength(255)
+                .WithMessage("Description should be not greater than 255 characters");
+
+            RuleFor(c => c.ProviderId).GreaterThan(0).WithMessage("Provider ID cannot be negative");
+
+        }
+    }
+}
