0caca3b770be926efcdd27833cf46ad4e01e38e4
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:23:36 +0300

    Create OriginCountryConfiguration.cs

---
+++ Library.Core/Persistence/EntityConfigurations/OriginCountryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Library.Core.Concrete.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Library.Core.Persistence.EntityConfigurations
+{
+    public class OriginCountryConfiguration :IEntityTypeConfiguration<OriginCountry>
+    
+    {
+        public void Configure(EntityTypeBuilder<OriginCountry> builder)
+        {
+            builder.Property(oc => oc.Id)
+                .IsRequired();
+
+            builder.Property(oc => oc.Name)
+                .HasMaxLength(255)
+                .IsRequired();
+            
+           builder
+                .HasMany(c => c.Providers)
+                .WithOne(p => p.OriginCountry)
+                .HasForeignKey(p => p.OriginCountryId);
+        }
+    }
+}
