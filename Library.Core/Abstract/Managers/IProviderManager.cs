e5e9e5d4dc514d14c520e32c8d887fc8830f59bb
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:22:50 +0300

    Create IProviderManager.cs

---
+++ Library.Core/Abstract/Managers/IProviderManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Library.Core.Concrete.Models;
+
+namespace Library.Core.Abstract.Managers
+{
+    public interface IProviderManager : IManager
+    {
+          IEnumerable<Provider> GetAllProviders();
+          
+          Provider GetProviderById(int id);
+       
+          Provider  FindProvider (int id);
+       
+          Provider  RemoveProvider (int id);
+    }
+}
