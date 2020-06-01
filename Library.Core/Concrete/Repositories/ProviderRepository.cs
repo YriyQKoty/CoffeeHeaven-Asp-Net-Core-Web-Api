8f6a4b1a2b63501d5ab02c0001ac18e4ded2e0f5
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:23:26 +0300

    Create ProviderRepository.cs

---
+++ Library.Core/Concrete/Repositories/ProviderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Library.Core.Abstract.Repositories;
+using Library.Core.Concrete.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Core.Concrete.Repositories
+{
+    public class ProviderRepository : Repository<Provider>, IProviderRepository
+    {
+        public ProviderRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
