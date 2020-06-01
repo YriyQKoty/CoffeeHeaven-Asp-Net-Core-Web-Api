497175cbc4b9cd1987d6c8596cdfdcda29df653d
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:23:32 +0300

    Create ApplicationDbContext.cs

---
+++ Library.Core/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Library.Core.Concrete.Models;
+using Library.Core.Persistence.EntityConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Core.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Coffee> Coffees { get; set; }
+        public DbSet<Provider> Providers { get; set; }
+        public DbSet<OriginCountry> Countries { get; set; }
+        
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CoffeeConfiguration());
+
+            modelBuilder.ApplyConfiguration(new ProviderConfiguration());
+
+            modelBuilder.ApplyConfiguration(new OriginCountryConfiguration());
+        }
+    }
+}
