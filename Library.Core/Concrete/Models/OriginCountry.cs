72ec14b90be98634aa2323848b2a85ca17cbd7b9
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:23:18 +0300

    Create OriginCountry.cs

---
+++ Library.Core/Concrete/Models/OriginCountry.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+
+namespace Library.Core.Concrete.Models
+{
+    public class OriginCountry
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public ICollection<Provider> Providers { get; set; }
+    }
+}
