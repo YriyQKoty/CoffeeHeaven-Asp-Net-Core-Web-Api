7c12c713926cc0978b73b1533f82711455e262d6
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:22:41 +0300

    Create OriginCountryRequestValidator.cs

---
+++ Library.Api/Validators/OriginCountryRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Library.Api.Requests;
+
+namespace Library.Api.Validators
+{
+    public class OriginCountryRequestValidator : AbstractValidator<OriginCountryRequest>
+    {
+        public OriginCountryRequestValidator()
+        {
+            RuleFor(p => p.Id).GreaterThan(0).WithMessage("Provider ID cannot be negative");
+            
+            RuleFor(p => p.Name).MinimumLength(2).WithMessage("Name is too short");
+            RuleFor(p => p.Name).MaximumLength(64).WithMessage("Name is too long");
+            
+        }
+    }
+}
