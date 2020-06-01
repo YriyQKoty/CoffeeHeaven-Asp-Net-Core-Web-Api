51138ff617f172af701d2cffa2635d82c74b6718
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:22:43 +0300

    Create ProviderRequestValidator.cs

---
+++ Library.Api/Validators/ProviderRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using FluentValidation;
+using Library.Api.Requests;
+
+namespace Library.Api.Validators
+{
+    public class ProviderRequestValidator : AbstractValidator<ProviderRequest>
+    {
+        public ProviderRequestValidator()
+        {
+            RuleFor(p => p.Id).GreaterThan(0).WithMessage("Provider ID cannot be negative");
+            
+            RuleFor(p => p.Name).MinimumLength(3).WithMessage("Name is too short");
+            RuleFor(p => p.Name).MaximumLength(64).WithMessage("Name is too long");
+            
+            RuleFor(p => p.OriginCountryId).GreaterThan(0).WithMessage("Origin Country ID cannot be negative");
+        }
+    }
+}
