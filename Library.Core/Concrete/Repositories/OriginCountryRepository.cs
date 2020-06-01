ecd51e93c046468404f989e77d5af95abe0053fd
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:23:24 +0300

    Create OriginCountryRepository.cs

---
+++ Library.Core/Concrete/Repositories/OriginCountryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Library.Core.Abstract.Repositories;
+using Library.Core.Concrete.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Core.Concrete.Repositories
+{
+    public class OriginCountryRepository : Repository<OriginCountry>, IOriginCountryRepository
+    {
+        public OriginCountryRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
