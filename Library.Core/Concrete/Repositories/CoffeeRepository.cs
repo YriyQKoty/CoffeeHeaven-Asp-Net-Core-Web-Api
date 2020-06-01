6c478a942be3170a5e3315db9d3b116f7e86af59
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:23:22 +0300

    Create CoffeeRepository.cs

---
+++ Library.Core/Concrete/Repositories/CoffeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Library.Core.Abstract.Repositories;
+using Library.Core.Concrete.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Core.Concrete.Repositories
+{
+    public class CoffeeRepository : Repository<Coffee>, ICoffeeRepository
+    {
+        public CoffeeRepository(DbContext context) : base(context)
+                 {
+                 }
+    }
+}
