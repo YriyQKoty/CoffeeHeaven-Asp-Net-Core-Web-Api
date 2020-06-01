8487f9136a0a6682c58f34dc1bbe4cca4dd7204c
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:23:10 +0300

    Create ProviderManager.cs

---
+++ Library.Core/Concrete/Managers/ProviderManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Library.Core.Abstract.Managers;
+using Library.Core.Abstract.Repositories;
+using Library.Core.Concrete.Models;
+
+namespace Library.Core.Concrete.Managers
+{
+    public class ProviderManager : IProviderManager
+    {
+        private readonly IProviderRepository _providerRepository;
+
+        public ProviderManager(IProviderRepository providerRepository)
+        {
+            _providerRepository = providerRepository;
+        }
+
+        public IEnumerable<Provider> GetAllProviders()
+        {
+            return _providerRepository.GetAll();
+        }
+        public Provider GetProviderById(int id)
+        {
+            return _providerRepository.Get(id);
+        }
+        
+        public Provider FindProvider(int id)
+        {
+            return _providerRepository.SingleOrDefault(provider => provider.Id == id);
+        }
+        
+        public Provider RemoveProvider(int id)
+        {
+            return _providerRepository.SingleOrDefault(provider => provider.Id == id);
+        }
+      
+        public int SaveChanges()
+        {
+            return _providerRepository.SaveChanges(); 
+        }
+        
+    }
+}
