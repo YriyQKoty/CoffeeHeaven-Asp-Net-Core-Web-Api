b4f4bffbea0ffc273608f567e267a77828dd0fbb
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:22:35 +0300

    Create ProviderResponse.cs

---
+++ Library.Api/Responses/ProviderResponse.cs
@@ -0,0 +1,14 @@
+﻿using Library.Core.Concrete.Models;
+
+namespace Library.Api.Responses
+{
+    public class ProviderResponse
+    {
+
+        public int Id { get; set; }
+      
+        public string Name { get; set; }
+
+        public int OriginCountryId { get; set; }
+    }
+}
