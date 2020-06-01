239e757cef9c341a7dc1eaca08122b15345ab216
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:23:20 +0300

    Create Provider.cs

---
+++ Library.Core/Concrete/Models/Provider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Library.Core.Concrete.Models
+{
+    public class Provider
+    {
+        
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public int OriginCountryId { get; set; }
+        
+        public OriginCountry OriginCountry{ get; set; }
+        public ICollection<Coffee> Coffees { get; set; }
+        
+    }
+}
