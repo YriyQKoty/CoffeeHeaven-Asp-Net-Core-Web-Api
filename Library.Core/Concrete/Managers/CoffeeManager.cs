14d489f7cf5038faafe109f4061a19641d2e5e03
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:23:04 +0300

    Create CoffeeManager.cs

---
+++ Library.Core/Concrete/Managers/CoffeeManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Library.Core.Abstract.Managers;
+using Library.Core.Abstract.Repositories;
+using Library.Core.Concrete.Models;
+using Library.Core.Concrete.Repositories;
+
+namespace Library.Core.Concrete.Managers
+{
+    public class CoffeeManager : ICoffeeManager
+    {
+        private readonly ICoffeeRepository _coffeeRepository;
+
+        public CoffeeManager(ICoffeeRepository coffeeRepository)
+        {
+            _coffeeRepository = coffeeRepository;
+        }
+
+        public IEnumerable<Coffee> GetAllCoffees()
+        {
+            return _coffeeRepository.GetAll();
+        }
+        
+        public Coffee GetCoffee(int id)
+        {
+            return _coffeeRepository.Get(id);
+        }
+        
+        public Coffee ChangeCoffee(int id)
+        {
+            return _coffeeRepository.SingleOrDefault(coffee => coffee.Id == id);
+        }
+        
+        public Coffee RemoveCoffee(int id)
+        {
+            return _coffeeRepository.SingleOrDefault(coffee => coffee.Id == id);
+        }
+        public int SaveChanges()
+        {
+            return _coffeeRepository.SaveChanges();
+;        }
+    }
+}
