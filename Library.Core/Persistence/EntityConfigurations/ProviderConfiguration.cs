30f9d4e2f10abe77ede00330da666bff99faebcf
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:23:38 +0300

    Create ProviderConfiguration.cs

---
+++ Library.Core/Persistence/EntityConfigurations/ProviderConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Library.Core.Concrete.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Library.Core.Persistence.EntityConfigurations
+{
+    public class ProviderConfiguration :IEntityTypeConfiguration<Provider>
+    {
+        public void Configure(EntityTypeBuilder<Provider> builder)
+        {
+            builder.Property(p => p.Id)
+                .IsRequired();
+            
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(511);
+            
+            builder
+                .HasMany(p => p.Coffees)
+                .WithOne(c => c.Provider)
+                .HasForeignKey(c => c.ProviderId);
+
+        }
+    }
+}
