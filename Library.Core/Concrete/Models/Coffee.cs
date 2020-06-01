a4eccead5ad15a2eed81620a7b907cb6f8b2f345
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:23:12 +0300

    Create Coffee.cs

---
+++ Library.Core/Concrete/Models/Coffee.cs
@@ -0,0 +1,21 @@
+﻿﻿namespace Library.Core.Concrete.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public float Price { get; set; }
+
+        public string Description { get; set; }
+        
+        public CoffeeType Type { get; set; }
+
+        public CoffeeQuality Quality { get; set; }
+
+        public int ProviderId { get; set; }
+
+        public Provider Provider { get; set; }
+
+    }
+}
