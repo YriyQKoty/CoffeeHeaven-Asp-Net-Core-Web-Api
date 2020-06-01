c64dd92c79a73bc0b293ff3377d3ae1b27dab70b
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:22:27 +0300

    Create CoffeeRequest.cs

---
+++ Library.Api/Requests/CoffeeRequest.cs
@@ -0,0 +1,26 @@
+﻿﻿using System.Runtime.Serialization;
+ using Library.Core.Concrete.Models;
+
+ namespace Library.Api.Requests
+{
+ 
+    public class CoffeeRequest
+    {
+      
+        public int Id { get; set; }
+       
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+  
+        public float Price { get; set; }
+  
+        public CoffeeType Type { get; set; }
+ 
+        public CoffeeQuality Quality { get; set; }
+        
+        public int ProviderId { get; set; }
+
+
+    }
+}
