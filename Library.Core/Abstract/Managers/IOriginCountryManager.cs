fa08122fa76906a8312d41f1f7c5e25674164aea
YriyQKoty <56413891+YriyQKoty@users.noreply.github.com>
2020-06-01 17:22:48 +0300

    Create IOriginCountryManager.cs

---
+++ Library.Core/Abstract/Managers/IOriginCountryManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Library.Core.Concrete.Models;
+
+namespace Library.Core.Abstract.Managers
+{
+    public interface IOriginCountryManager : IManager
+    {
+        IEnumerable<OriginCountry> GetAllCountries();
+          
+        OriginCountry GetCountryById(int id);
+       
+        OriginCountry  FindCountry (int id);
+       
+        OriginCountry  RemoveCountry (int id);
+    }
+}
